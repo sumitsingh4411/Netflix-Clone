8867fa0717ac3452140e5de543ddb659948cff56
sumitsingh4411 <shivsingh6371913458@gmail.com>
2021-01-05 16:20:40 +0530

    this is netflix clone
    this is netflix clone

---
+++ README.md
@@ -0,0 +1,2 @@
+# Netflix-clone
+ this is netflix clone
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Row from "./Row";
+import request from "./request";
+import Banner from "./Banner";
+import Nav from "./Nav";
+import  "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+    <Nav />
+    <Banner />
+    <Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={request.fetchNetflixOriginals} />
+    <Row title="Trending" fetchUrl={request.fetchTrending} />
+    <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+    <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+    <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+    <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+    <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+    <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+  </div>
+  );
+}
+
+export default App;
